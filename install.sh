b8bea3a64b6a46ce4bb070e1c5ceddda791bf289
d3rf <derf974@gmail.com>
2019-04-09 23:53:38 +0200

    add restart enable modules

--- install.sh
+++ install.sh
@@ -2,17 +2,19 @@
 set -e 
 
 info(){
-    echo -e "\e[93m-> $1 \e[39"
+    echo -e "\e[93m-> $1 \e[39m"
 }
 
 
-info "-> Deinstall phpmyadmin apache2 mysql-server" 
-apt purge -y phpmyadmin
-apt purge -y apache2
-apt purge -y mysql-server
+info "-> Deinstallation phpmyadmin apache2 mysql-server" 
+apt purge -y phpmyadmin apache2 mysql-server
 
+info "-> Installation phpmyadmin apache2 mysql-server"
+apt install -y apache2 php libapache2-mod-php mysql-server php-mysql phpmyadmin
+info "-> deactivation mpm_event"
+a2dismod mpm_event
+info "-> activation php"
+a2enmod php7.2
+info "-> Redemarrage d'apache"
+service apache2 restart
 
-info "-> Install phpmyadmin apache2 mysql-server"
-apt install -y mysql-server 
-apt install -y apache2
-apt install -y phpmyadmin
